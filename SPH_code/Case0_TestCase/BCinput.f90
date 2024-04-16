8807139a9b190bd9b8146e84a52bbfa704f8da5b
parikshit <boregowdaparikshit@gmail.com>
2024-04-16 19:28:55 -0400

    Docs: etype_itype.text document added

---
+++ SPH_code/Case0_TestCase/BCinput.f90
@@ -0,0 +1,20 @@
+!****************************************************************************
+!
+!  SUBROUTINE: BCinput
+!
+!  PURPOSE:  Subroutine that allows imposing different types of BC for 
+!               for different boundary groups
+!
+!****************************************************************************
+subroutine BCinput(etype, tempType)
+
+    if(tempType .eq. 1) etype = 3
+        
+    if(tempType .eq. 2) etype = 3
+    
+    if(tempType .eq. 3) etype = 3
+        
+    if(tempType .eq. 3) etype = 3  
+
+
+end subroutine
